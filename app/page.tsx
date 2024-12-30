86b246d334f815ac918cc0eb387d9cc87651a99a
umarGit <umarsheikh303@gmail.com>
2024-12-30 13:10:39 +0300

    feat(add): added content

--- app/page.tsx
+++ app/page.tsx
@@ -15,26 +15,142 @@ export default function Home() {
           </section>
           <section className="flex-1 px-4 pt-4 border-l border-t rounded-tl-lg border-neutral-400 h-[calc(100vh-16px)]">
             <div className="overflow-auto hide-scrollbar h-full flex flex-col justify-between">
-              <div className="text-lg text-neutral-300">
-                <p className="mb-4">
-                  <strong>Senior Software Engineer</strong> with a flair for
-                  simplifying complexity.
-                </p>
-                <p className="mb-4">
-                  <strong>Currently:</strong> Pursuing a Master's in Big Data &
-                  Machine Learning at ITMO University.
-                </p>
-                <p>
-                  <strong>Previously:</strong> Led scalable innovations at
-                  Aeroglobe and digital transformations at Webnorth.
-                </p>
+              <div className="text-md text-neutral-300 space-y-6">
+                {/* Summary */}
+                <div>
+                  <h3 className="text-xl font-semibold text-neutral-200 mb-2">
+                    <span className="highlight text-red-400">import</span>{" "}
+                    introduction{" "}
+                    <span className="highlight text-red-400">from</span> life
+                  </h3>
+                  <p>
+                    <strong>Senior Software Engineer</strong> with a knack for
+                    breaking down complex systems into innovative, scalable
+                    solutions. A relentless problem-solver driven by the pursuit
+                    of excellence and measurable impact.{" "}
+                    <code className="text-sm bg-neutral-200 bg-opacity-15 px-1 py-[1px] rounded-sm">
+                      debug(this);
+                    </code>
+                  </p>
+                </div>
+
+                {/* Current Endeavors */}
+                <div>
+                  <h3 className="text-xl font-semibold text-neutral-200 mb-2">
+                    <span className="highlight text-green-400">const</span>{" "}
+                    Currently
+                  </h3>
+                  <ul className="list-disc pl-5 space-y-2">
+                    <li>
+                      Pursuing a{" "}
+                      <span className="text-green-400">Master's</span> in
+                      <span className="highlight text-green-400">
+                        Big Data
+                      </span>{" "}
+                      and
+                      <span className="highlight text-green-400">
+                        Machine Learning
+                      </span>{" "}
+                      at
+                      <span className="highlight text-yellow-400">
+                        ITMO University
+                      </span>
+                      .
+                    </li>
+                    <li>
+                      <code className="text-sm bg-neutral-200 bg-opacity-15 px-1 py-[1px] rounded-sm">
+                        tackleRealWorldChallenges()
+                      </code>{" "}
+                      with AI-driven solutions, bridging academia and practical
+                      innovation.
+                    </li>
+                  </ul>
+                </div>
+
+                {/* Previous Experience */}
+                <div>
+                  <h3 className="text-xl font-semibold text-neutral-200 mb-2">
+                    <span className="highlight text-blue-400">class</span>{" "}
+                    Previously
+                  </h3>
+                  <ul className="list-disc pl-5 space-y-2">
+                    <li>
+                      Transformed scalability and performance at
+                      <span className="highlight text-blue-400">Aeroglobe</span>
+                      , implementing lazy-loading to reduce load times by 90%.{" "}
+                      <code className="text-sm bg-neutral-200 bg-opacity-15 px-1 py-[1px] rounded-sm">
+                        optimize();
+                      </code>
+                    </li>
+                    <li>
+                      Spearheaded high-impact digital projects at
+                      <span className="highlight text-green-400">Webnorth</span>
+                      , developing solutions tailored for diverse user needs.{" "}
+                      <code className="text-sm bg-neutral-200 bg-opacity-15 px-1 py-[1px] rounded-sm">
+                        refactor(UX);
+                      </code>
+                    </li>
+                  </ul>
+                </div>
+
+                {/* Key Accomplishments */}
+                <div>
+                  <h3 className="text-xl font-semibold text-neutral-200 mb-2">
+                    <span className="highlight text-orange-400">fn</span> Key
+                    Accomplishments
+                  </h3>
+                  <ul className="list-disc pl-5 space-y-2">
+                    <li>
+                      Migrated a micro-frontend architecture to a monolith,
+                      achieving a 30% improvement in maintainability.{" "}
+                      <code className="text-sm bg-neutral-200 bg-opacity-15 px-1 py-[1px] rounded-sm">
+                        cleanCode();
+                      </code>
+                    </li>
+                    <li>
+                      Reduced rendering times, ensuring a consistent 60fps
+                      experience across applications.{" "}
+                      <code className="text-sm bg-neutral-200 bg-opacity-15 px-1 py-[1px] rounded-sm">
+                        optimizeRendering();
+                      </code>
+                    </li>
+                    <li>
+                      Delivered a medical database solution in Denmark,
+                      revolutionizing accessibility for practitioners and
+                      patients.{" "}
+                      <code className="text-sm bg-neutral-200 bg-opacity-15 px-1 py-[1px] rounded-sm">
+                        innovateHealthcare();
+                      </code>
+                    </li>
+                  </ul>
+                </div>
+
+                {/* Languages and Countries */}
+                <div>
+                  <h3 className="text-xl font-semibold text-neutral-200 mb-2">
+                    <span className="highlight text-purple-400">var</span>{" "}
+                    Languages & Countries
+                  </h3>
+                  <ul className="list-disc pl-5 space-y-2">
+                    <li>
+                      <strong>Languages:</strong> English, Russian, Arabic, Urdu
+                    </li>
+                    <li>
+                      <strong>Countries I've Lived or Worked In:</strong>{" "}
+                      Singapore, Doha, Abu Dhabi, Russia
+                    </li>
+                  </ul>
+                </div>
               </div>
-              <div className="text-sm text-neutral-500 mb-4">
+
+              <div className="text-sm text-neutral-500 mt-4 mb-16">
                 <p>Contact: umarsheikh303@gmail.com | +7 999 217 24 11</p>
                 <p>
                   Find me on LinkedIn:{" "}
                   <a
                     href="https://linkedin.com/in/umarsheikh303"
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className="underline"
                   >
                     /in/umarsheikh303
@@ -47,6 +163,9 @@ export default function Home() {
                   </small>
                 </footer>
               </div>
+
+              <div className="w-full h-12 bg-gradient-to-b from-neutral-900 to-transparent absolute top-[1px]"></div>
+              <div className="w-full h-24 bg-gradient-to-t from-neutral-900 to-transparent absolute bottom-0"></div>
             </div>
           </section>
           <section className="absolute border-t border-neutral-400 w-full -right-full"></section>
